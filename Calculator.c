53a78165e8b745bf10842c4511191e2c3f3a0244
aneesh1024 <99677867+aneesh1024@users.noreply.github.com>
2022-02-26 22:01:17 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,37 +1,48 @@
 #include<stdio.h>
+#include<math.h>
 int fact(int x);
-int main()
+void main()
 {
-    float a,b;
+    double a,b;
     char fun,choice;
-    printf("THIS IS A SIMPLE CALCULATOR DEVELOPED BY ANEESH SHARMA AND CUURRENTLY IT SUPPORTS FIVE FUNCTIONS\n");
+    printf("THIS IS A SIMPLE CALCULATOR DEVELOPED BY ANEESH SHARMA AND CUURRENTLY IT SUPPORTS SEVEN FUNCTIONS\n");
     do
     {
     printf("\nEnter the first number:");
-    scanf("%f",&a);
+    scanf("%lf",&a);
     getchar();
-    printf("Enter the function(+,-,*,/,!):");
+    printf("Enter the function(+,-,*,/,^[power],![factorial],s[square root]:");
     scanf("%c",&fun);
-    if(fun=='!')
+    if(fun=='+' || fun=='-' || fun=='*' || fun=='/' || fun=='!' || fun=='s' || fun=='^')
     {
-         int f=fact(a);
-         printf("%d!=%d",(int)a,(int)f);
+    if(fun=='!' || fun=='s')
+    {
+        if(fun=='!')
+        {
+            int f=fact(a);
+            printf("%d!=%d",(int)a,(int)f);
+        }
+        else
+        {
+            double sr=sqrt(a);
+            printf("sqrt(%.3lf)=%.3lf",a,sr);
+        }
     }
     else
     {
     printf("Enter the second number:");
-    scanf("%f",&b);
+    scanf("%lf",&b);
     printf("\n");
     switch(fun)
     {
     case '+':
-        printf("%f+%f=%f",a,b,a+b);
+        printf("%.3lf+%.3lf=%.3lf",a,b,a+b);
         break;
     case '-':
-        printf("%f-%f=%f",a,b,a-b);
+        printf("%.3lf-%.3lf=%.3lf",a,b,a-b);
         break;
     case '*':
-        printf("%f*%f=%f",a,b,a*b);
+        printf("%.3lf*%.3lf=%.3lf",a,b,a*b);
         break;
     case '/':
         if(b==0)
@@ -40,12 +51,20 @@ int main()
         }
         else
         {
-            printf("%f/%f=%f",a,b,a/b);
+            printf("%.3lf/%.3lf=%.3lf",a,b,a/b);
         }
+        break;
+    case '^':
+        printf("(%.3lf)^%.3lf=%.3lf",a,b,pow(a,b));
         break;
     default:
         printf("\nWrong input !!!");
     }
+    }
+    }
+    else
+    {
+        printf("Wrong input !!!");
     }
     getchar();
     printf("\n\nDo yo want to calculate again(Enter y for yes and n for no):");
@@ -64,4 +83,3 @@ int fact(int x)
         return x*fact(x-1);
     }
 }
-
