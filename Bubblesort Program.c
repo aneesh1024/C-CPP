302ff2e6c038ffc1efcfdf6975e223ad4bcff8ef
aneesh1024 <99677867+aneesh1024@users.noreply.github.com>
2022-03-14 23:19:38 +0530

    Update Bubblesort Program.c

--- Bubblesort Program.c
+++ Bubblesort Program.c
@@ -2,11 +2,11 @@
 void printarray(int* arr, int n)
 {
     printf("{");
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n-1; i++)
     {
         printf("%d, ",arr[i]);
     }
-    printf("}");
+     printf("%d}",arr[n-1]);
 }
 void bubblesort(int *arr, int n)
 {
@@ -16,7 +16,7 @@ void bubblesort(int *arr, int n)
         {
             if(arr[j]>arr[j+1])
             {
-                int temp;
+                 int temp;
                  temp=arr[j];
                  arr[j]=arr[j+1];
                  arr[j+1]=temp;
@@ -24,20 +24,29 @@ void bubblesort(int *arr, int n)
         }
     }
 }
-int main()
+void main()
 {
-    int arr[10];
-    int n=10;
-    printf("Enter the array:\n");
-    for(int i=0; i<n; i++)
+    int arr[100];
+    int size;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    char choice;
+    do
     {
-        printf("Enter arr[%d]:",i);
-        scanf("%d",&arr[i]);
+            printf("Enter the array:\n");
+        for(int i=0; i<size; i++)
+        {
+            printf("Enter arr[%d]:",i);
+            scanf("%d",&arr[i]);
+        }
+        printarray(arr, size);
+        printf("\n");
+        bubblesort(arr, size);
+        printf("\nSorted Array:");
+        printarray(arr, size);
+        getchar();
+        printf("\nDo you want to exit(Enter n for no and y for yes):");
+        scanf("%c",&choice);
     }
-    printarray(arr, n);
-    printf("\n");
-    bubblesort(arr, n);
-    printf("\nSorted Array:");
-    printarray(arr, n);
-    return 0;
+    while(choice=='n');
 }
