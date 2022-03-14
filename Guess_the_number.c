c74609543224f9e75a6725b14f4edaf269593e2a
aneesh1024 <99677867+aneesh1024@users.noreply.github.com>
2022-03-14 23:23:15 +0530

    Update Guess_the_number.c

--- Guess_the_number.c
+++ Guess_the_number.c
@@ -3,38 +3,39 @@
 #include<time.h>
 int main()
 {
-    int number,nguess=1;
+    int number,nguess;
     int gnumber;
     char choice;
     do
     {
-    srand(time(0));
-    number=rand()%100 + 1;
-    do
-    {
-        printf("\nGuess the number between 1 and 100):");
-        scanf("%d",&gnumber);
-        if(gnumber>number)
+        nguess=1;
+        srand(time(0));
+        number=rand()%100 + 1;
+        do
         {
-            printf("Lower your number\n");
-        }
-        else
-        {
-            if(gnumber<number)
+            printf("\nGuess the number between 1 and 100:");
+            scanf("%d",&gnumber);
+            if(gnumber>number)
             {
-                printf("increase your number\n");
+                printf("Lower your number\n");
             }
             else
             {
-                printf("You have guessed the correct number in %d attempts !!!\n",nguess);
+                if(gnumber<number)
+                {
+                    printf("increase your number\n");
+                }
+                else
+                {
+                    printf("You have guessed the correct number in %d attempts !!!\n",nguess);
+                }
             }
+            nguess++;
         }
-        nguess++;
-    }
-    while(gnumber!=number);
-    getchar();
-    printf("Play Again( Enter y for yes an n for no:");
-    scanf("%c",&choice);
+        while(gnumber!=number);
+        getchar();
+        printf("\nPlay Again( Enter y for yes an n for no):");
+        scanf("%c",&choice);
     }
     while(choice=='y');
     return 0;
